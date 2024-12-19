17c4f8b6dbff697394a9c7fd0a4b9a82f1af11ab
djosix <djosicks@gmail.com>
2024-12-19 23:43:18 +0800

    init

---
+++ api.php
@@ -0,0 +1,39 @@
+<?php
+
+date_default_timezone_set('Asia/Taipei');
+header('Content-Type: text/plain');
+
+$db = new SQLite3('db.sqlite3');
+$db->exec('CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, date DATE, name TEXT, value FLOAT)');
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    header('Content-Type: application/json');
+    $result = $db->query('SELECT * FROM data');
+    $json = [];
+    while ($row = $result->fetchArray()) {
+        $name = $row['name'];
+        $date = $row['date'];
+        $value = $row['value'];
+        $json[$name][] = [
+            'date' => $date,
+            'value' => $value,
+        ];
+    }
+    die(json_encode($json));
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (
+        ($name = @$_POST['name']) &&
+        ($value = @$_POST['value']) &&
+        ('4eb252f1bf0216058b3c0b1b0cf57724' === md5(@$_POST['password'] . 'RjlQeQyqtDMvPUpg4e3kvaOelJJyxlhh'))
+    ) {
+        $statement = $db->prepare('INSERT INTO data VALUES (NULL, :date, :name, :value)');
+        $statement->bindValue(':date', date('Y-m-d H:i:s'), SQLITE3_TEXT);
+        $statement->bindValue(':name', $name, SQLITE3_TEXT);
+        $statement->bindValue(':value', $value, SQLITE3_FLOAT);
+        $statement->execute();
+        header('Location: .');
+        echo "Success\n";
+    } else {
+        echo "Error\n";
+    }
+}
